14dc42139b5015c08d5d839b732b8ec9e207287f
Nicolas WYSEUR <nicolaswyseur@gmail.com>
2024-04-02 12:36:04 +0200

    signup

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from "express";
+import authRouters from "./src/routes/auth-route";
 
 const app: Express = express();
+
+app.use(express.json());  // Middleware to parse JSON
+app.use(authRouters); // Use the authRouters
 
 app.get("/api", (req: Request, res: Response) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
